ec6e0850adcf0e654f4ea3206b930ef996d89ce3
zuoqiang <756465185@qq.com>
2018-11-08 23:03:44 +0800

    fix

--- src/main/java/org/throwable/entity/ProjectBill.java
+++ src/main/java/org/throwable/entity/ProjectBill.java
@@ -16,69 +16,116 @@ import java.util.concurrent.locks.Condition;
  * @since 2017/4/23 22:21
  */
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class ProjectBill {
 
 
-	private String userId;
-	private String orderNo;
-	private int currentPeroid;
-	private int totalPeroids;
-	private String amount;
+    private String userId;
+    private String orderNo;
+    private int currentPeroid;
+    private int totalPeroids;
+    private String amount;
 
-	public static void main(String[] args) throws Exception {
-		String userId = "userIdxxxxx";
-		String orderNo = "orderNoxxxxx";
-		String amount = "100";
-		int totalPeroids = 12;
-		final Object MONITOR = new Object();
-		List<ProjectBill> bills = Lists.newArrayList();
-		bills.add(new ProjectBill(userId, orderNo, 3, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 1, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 2, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 4, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 12, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 5, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 7, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 6, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 9, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 8, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 10, totalPeroids, amount));
-		bills.add(new ProjectBill(userId, orderNo, 11, totalPeroids, amount));
-		for (ProjectBill bill : bills) {
-			new Thread(new Comsumer(bill, MONITOR)).start();
-		}
-		Thread.sleep(Integer.MAX_VALUE);
-	}
+    public ProjectBill(String userId, String orderNo, int currentPeroid, int totalPeroids, String amount) {
+        this.userId = userId;
+        this.orderNo = orderNo;
+        this.currentPeroid = currentPeroid;
+        this.totalPeroids = totalPeroids;
+        this.amount = amount;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public int getCurrentPeroid() {
+        return currentPeroid;
+    }
+
+    public void setCurrentPeroid(int currentPeroid) {
+        this.currentPeroid = currentPeroid;
+    }
+
+    public int getTotalPeroids() {
+        return totalPeroids;
+    }
+
+    public void setTotalPeroids(int totalPeroids) {
+        this.totalPeroids = totalPeroids;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String userId = "userIdxxxxx";
+        String orderNo = "orderNoxxxxx";
+        String amount = "100";
+        int totalPeroids = 12;
+        final Object MONITOR = new Object();
+        List<ProjectBill> bills = Lists.newArrayList();
+        bills.add(new ProjectBill(userId, orderNo, 3, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 1, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 2, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 4, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 12, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 5, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 7, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 6, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 9, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 8, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 10, totalPeroids, amount));
+        bills.add(new ProjectBill(userId, orderNo, 11, totalPeroids, amount));
+        for (ProjectBill bill : bills) {
+            new Thread(new Comsumer(bill, MONITOR)).start();
+        }
+        Thread.sleep(Integer.MAX_VALUE);
+    }
 
 
-	private static class Comsumer implements Runnable {
+    private static class Comsumer implements Runnable {
 
-		private final ProjectBill bill;
+        private final ProjectBill bill;
 
-		private final Object monitor;
+        private final Object monitor;
 
-		public Comsumer(ProjectBill bill, Object monitor) {
-			this.bill = bill;
-			this.monitor = monitor;
-		}
+        public Comsumer(ProjectBill bill, Object monitor) {
+            this.bill = bill;
+            this.monitor = monitor;
+        }
 
-		@Override
-		public void run() {
-			try {
-				synchronized (monitor) {
-					if (bill.getCurrentPeroid() == bill.getTotalPeroids()) {
-						monitor.wait();
-					} else if (bill.getCurrentPeroid() == bill.getTotalPeroids() - 1) {
-						monitor.notifyAll();
-					}
-					System.out.println(bill.toString());
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        @Override
+        public void run() {
+            try {
+                synchronized (monitor) {
+                    if (bill.getCurrentPeroid() == bill.getTotalPeroids()) {
+                        monitor.wait();
+                    } else if (bill.getCurrentPeroid() == bill.getTotalPeroids() - 1) {
+                        monitor.notifyAll();
+                    }
+                    System.out.println(bill.toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
